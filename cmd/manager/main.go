f88af6a91af6c1249e9a5b43d72cba8ec49b4934
atticuss <jonn.callahan@gmail.com>
2020-08-23 18:06:10 -0400

    missed manager file on config file support

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -1,16 +1,66 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/atticuss/chefconnect/repositories/dgraph"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/spf13/viper"
 )
 
-func main() {
-	dgraphConfig := dgraph.Config{
-		Host: "ec2-34-238-150-16.compute-1.amazonaws.com:9080",
+type configuration struct {
+	Database struct {
+		Host string `envconfig:"DB_HOST"`
+		Port string `envconfig:"DB_PORT"`
 	}
-	utilRepo := dgraph.NewDgraphRepositoryUtility(&dgraphConfig)
+}
 
-	utilRepo.ClearDatastore()
-	utilRepo.InitializeSchema()
-	utilRepo.InitializeBaseData()
+func parseConfig() (*configuration, error) {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	var config configuration
+
+	if err := viper.ReadInConfig(); err != nil {
+		return &config, err
+	}
+	err := viper.Unmarshal(&config)
+	if err != nil {
+		return &config, err
+	}
+
+	err = envconfig.Process("", &config)
+	if err != nil {
+		return &config, err
+	}
+
+	return &config, nil
+}
+
+func main() {
+	config, err := parseConfig()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	dgraphConfig := dgraph.Config{
+		Host: fmt.Sprintf("%s:%s", config.Database.Host, config.Database.Port),
+	}
+	repo := dgraph.NewDgraphRepository(&dgraphConfig)
+
+	if err := repo.ClearDatastore(); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := repo.InitializeSchema(); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := repo.InitializeBaseData(); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := repo.InitializeTestData(); err != nil {
+		log.Fatal(err)
+	}
 }
