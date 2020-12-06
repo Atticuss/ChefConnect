0d8d887505dfbed9fb67278bf1110615113f2523
atticuss <jonn.callahan@gmail.com>
2020-12-06 17:36:55 -0500

    add "Authorization" header in cors resp

--- controllers/rest/rest.go
+++ controllers/rest/rest.go
@@ -146,7 +146,7 @@ func corsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Authorization")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 
 		if c.Request.Method == "OPTIONS" {
